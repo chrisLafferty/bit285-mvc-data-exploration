9de082a9e12cf34bd0e47245ff1a9d8bc0ce2246
chris <chris@CHRIS-PC>
2017-01-24 15:13:43 -0800

    database-first-complete

---
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_DataExploration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_DataExploration.Controllers
+{
+    public class ProductController : Controller
+    {
+        private MyDBContext db = new MyDBContext();
+       
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products);
+        }
+    }
+}
